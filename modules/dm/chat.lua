03a389002f5389f782c2e6303cb251ebd28e08f4
Roney <roney@roney.one>
2025-04-10 16:02:08 +0100

    Added ability to reply to messages

--- modules/dm/chat.lua
+++ modules/dm/chat.lua
@@ -8,6 +8,7 @@ local Config = json.decode(fs.readFileSync("./config.json"))
 local timer = require("timer")
 
 local channel
+local reply
 
 ---------------
 -- FUNCTIONS --
@@ -22,8 +23,54 @@ local function SendMessage(message,client)
 	end
 
 	-- If is configured, send the message
-	local chat = channel:send{content=message.content:sub(6)}
+	local reference = reply and {message=reply,mention=true} or nil
+	local chat = channel:send{content=message.content:sub(6),reference=reference}
 	local response = message.channel:send{content="Message sent: "..chat.link,reference={message=message,mention=false}}
+	reply = nil timer.sleep(5000) response.delete(response)
+end
+
+local function SetReply(message,client)
+	-- Check if message has a valid URL
+	local link = message.content:sub(7)
+	if link:sub(1,29) ~= "https://discord.com/channels/" then
+		local response = message.channel:send{content="Link not valid.",reference={message=message,mention=false}}
+		reply = nil timer.sleep(5000) response.delete(response)
+		return
+	end
+
+	-- Parse link to get guild ID, channel ID and message ID
+	local tbl = {}
+	for i in link:gmatch("([^/]+)") do table.insert(tbl,i) end
+
+	if not tbl[4] or not tbl[5] or not tbl[6] then
+		local response = message.channel:send{content="Couldn't parse URL.",reference={message=message,mention=false}}
+		reply = nil timer.sleep(5000) response.delete(response)
+		return
+	end
+
+	-- Get guild object
+	reply = client:getGuild(tbl[4])
+	if not reply then
+		local response = message.channel:send{content="Couldn't get guild.",reference={message=message,mention=false}}
+		reply = nil timer.sleep(5000) response.delete(response)
+	end
+
+	-- Get channel object
+	reply = reply:getChannel(tbl[5])
+	if not reply then
+		local response = message.channel:send{content="Couldn't get channel.",reference={message=message,mention=false}}
+		reply = nil timer.sleep(5000) response.delete(response)
+	end
+
+	-- Get message object
+	reply = reply:getMessage(tbl[6])
+	if not reply then
+		local response = message.channel:send{content="Couldn't get message.",reference={message=message,mention=false}}
+		reply = nil timer.sleep(5000) response.delete(response)
+	end
+
+	-- Feedback that everything is good
+	local response = message.channel:send{content="Replying to message: "..reply.link,reference={message=message,mention=false}}
 	timer.sleep(5000) response.delete(response)
 end
 
@@ -61,7 +108,7 @@ local function SetTextChannel(message,client)
 	end
 
 	-- Feedback that everything is good
-	local response = message.channel:send{content="Current channel: "..channel.mentionString,reference={message=message,mention=false}}
+	local response = message.channel:send{content="Sending message to channel: "..channel.mentionString,reference={message=message,mention=false}}
 	timer.sleep(5000) response.delete(response)
 end
 
@@ -77,6 +124,7 @@ local function Chat(message,client) -- messageCreate event
 
 	if message.content:sub(1,5):lower() == "chat " then SendMessage(message,client) end
 	if message.content:sub(1,6):lower() == "chatc " then SetTextChannel(message,client) end
+	if message.content:sub(1,6):lower() == "chatr " then SetReply(message,client) end
 end
 
 -------------------
